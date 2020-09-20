f12f9ceafd961ec92c24a1ab5baf6b963d13e16c
AcHumphrey <AcHumphrey@example.com>
2020-09-20 23:46:11 +0100

    add method

--- app/src/main/java/com/example/drawerlayoutandlistviewjava/MainActivity.java
+++ app/src/main/java/com/example/drawerlayoutandlistviewjava/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -100,6 +101,13 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.e("MainActivity", "Error in creating fragment");
         }
+    }
+
+    // Show the menu on the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
