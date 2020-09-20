b61b280b82bff66621352e21e5b21d1c570378ef
AcHumphrey <AcHumphrey@example.com>
2020-09-20 23:34:47 +0100

    initial commit

---
+++ app/src/main/java/com/example/drawerlayoutandlistviewjava/FixturesFragment.java
@@ -0,0 +1,33 @@
+package com.example.drawerlayoutandlistviewjava;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FixturesFragment extends Fragment {
+
+
+    public FixturesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_fixtures, container, false);
+
+        return rootView;
+
+    }
+
+}
